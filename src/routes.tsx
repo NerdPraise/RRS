594d0d1001a5631103e29e6b556f6dcdad7631c5
NerdPraise <praiseajayi2@gmail.com>
2024-01-10 11:57:41 +0100

    Fix: Deploy config

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import { ResultsPage } from '@Pages/ResultsPage'
 import { CreationPage } from '@Pages/Article/CreationPage'
 
 const ROUTES = [
   {
-    path: '/article',
-    element: <CreationPage />,
+    path: '/',
+    element: (
+      <div>
+        Welcome, <Link to="/results">Results</Link> or{' '}
+        <Link to="/article">Articles</Link>{' '}
+      </div>
+    ),
   },
   {
     path: '/results',
     element: <ResultsPage />,
+  },
+  {
+    path: '/article',
+    element: <CreationPage />,
   },
 ]
 
